5de565bb8606c48bdc6a499a6241a7724f74b24c
riarhmawati <60091247+riarhmawati@users.noreply.github.com>
2020-11-15 22:37:47 +0700

    4

--- MODUL3 RIA/Detail_Event.php
+++ MODUL3 RIA/Detail_Event.php
@@ -39,7 +39,7 @@
         <h2 class="text-center" style="color: DodgerBlue;">DETAILS EVENT!</h2>
         <div class="container-card" style="width: 50rem;margin: auto;padding-top: 30px;">
             <div class="card">
-                <img src="gambar/<?= $event['gambar'] ?>" class="card-image-top" style="max-height: 400px; max-widht: 400px;">
+                <img src="gambar/<?= $event['gambar'] ?>" class="card-image-top" style="max-height: 500px;">
             <div class="card-body" style="line-height: 70%;">
                 <h3><?= $event["name"] ?></h3>
                 <p class="font-weight-bold">Deskripsi</p>
@@ -47,7 +47,7 @@
                 <div class="row">
                     <div class="col col-6">
                         <p class="font-weight-bold">Informasi Event</p>
-                           <p>
+                        <p>
                            <i class="fa fa-calendar" style="color: orange"></i>
                             <?= $event["tanggal"] ?>
                         </p>
@@ -95,108 +95,107 @@
                     <form action="Update.php" method="POST" enctype="multipart/form-data">
                         <input type="text" name="id" value="<?= $event['id'] ?>" hidden>
                         <div class="row">
-            <div class="col col-6" style="background-color:whitesmoke;height: 600px;">
-            <div class="card h-100">
-            <div style="height: 24px; border-radius: 4px 4px 0 0" class="bg-danger"></div>
-            <div class="card-body">
+                            <div class="col col-6" style="background-color:whitesmoke;height: 600px;">
+                            <div class="card h-100">
+                                <div style="height: 24px; border-radius: 4px 4px 0 0" class="bg-danger"></div>
+                                <div class="card-body">
 
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" value="<?= $event["name"] ?>">
-                </div>
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" class="form-control" id="name" name="name" value="<?= $event["name"] ?>">
+                                </div>
 
-                <div class="form-group md-outline input-with-post-icon datepicker">
-                    <label for="deskripsi">Deskripsi</label>
-                    <textarea class="form-control" id="deskripsi" rows="3" name="deskripsi"><?= $event["deskripsi"] ?></textarea>
-                </div>
+                                <div class="form-group md-outline input-with-post-icon datepicker">
+                                    <label for="deskripsi">Deskripsi</label>
+                                <textarea class="form-control" id="deskripsi" rows="3" name="deskripsi"><?= $event["deskripsi"] ?></textarea>
+                                </div>
 
-                <div class="form-group">
-                <label for="gambar">Upload Gambar</label>   
-                <div class="custom-file">
+                                <div class="form-group">
+                                    <label for="gambar">Upload Gambar</label>   
+                                <div class="custom-file">
                     
-                    <input type="file" class="custom-file-input" id="gambar" name="gambar">
-                    <label class="custom-file-label" for="gambar">Choose file</label>
-                    </div>
-                </div>
-// karena text area, ga ada atribut <textarea> ... </textarea>
+                                    <input type="file" class="custom-file-input" id="gambar" name="gambar">
+                                    <label class="custom-file-label" for="gambar">Choose file</label>
+                                </div>
+                                </div>
 
-                <div class="form-group ">
-                <legend class="col-form-label ">Kategori</legend>
+
+                                <div class="form-group ">
+                                <legend class="col-form-label ">Kategori</legend>
                     
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="kategori" id="kategori" value="online" checked>
-                    <label class="form-check-label" for="kategori"> Online </label>
-                </div>
-                <div class="form-check form-check-inline">                
-                    <input class="form-check-input" type="radio" name="kategori" id="kategori" value="offline">
-                    <label class="form-check-label" for="kategori">Offline</label>
-                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="kategori" id="kategori" value="online" checked>
+                                    <label class="form-check-label" for="kategori"> Online </label>
+                                </div>
+                                <div class="form-check form-check-inline">                
+                                    <input class="form-check-input" type="radio" name="kategori" id="kategori" value="offline">
+                                    <label class="form-check-label" for="kategori">Offline</label>
+                                </div>
                 
-            </div>
-            </div>
-            </div>
-            </div>
-            <div class="col col-6" style="background-color:whitesmoke;height: 600px;">
-            <div class="card h-100">
-            <div style="height: 24px; border-radius: 4px 4px 0 0" class="bg-primary"></div>
-            <div class="card-body">    
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="col col-6" style="background-color:whitesmoke;height: 600px;">
+                            <div class="card h-100">
+                                <div style="height: 24px; border-radius: 4px 4px 0 0" class="bg-primary"></div>
+                                <div class="card-body">    
             
-                <div class="form-group md-outline input-with-post-icon datepicker">        
-                    <label for="tanggal">Tanggal</label>
-                    <input type="date" class="form-control" id="tanggal" name="tanggal" value=" <?= $event["tanggal"] ?>" >  
-                </div>
+                                <div class="form-group md-outline input-with-post-icon datepicker">        
+                                    <label for="tanggal">Tanggal</label>
+                                    <input type="date" class="form-control" id="tanggal" name="tanggal" value=" <?= $event["tanggal"] ?>" >  
+                                </div>
 
-                <div class="form-row align-items-center">
-                    <div class="form-group col-md-6"> 
-                        <label for="mulai">Jam Mulai</label>
-                        <input type="time" id="mulai" class="form-control" name="mulai" value="<?= $event["mulai"] ?>">
-                    </div>
+                                <div class="form-row align-items-center">
+                                <div class="form-group col-md-6"> 
+                                    <label for="mulai">Jam Mulai</label>
+                                    <input type="time" id="mulai" class="form-control" name="mulai" value="<?= $event["mulai"] ?>">
+                                </div>
 
-                    <div class="form-group col-md-6">
-                        <label for="berakhir">Jam Berakhir</label>
-                        <input type="time" id="berakhir" class="form-control" name="berakhir" value="<?= $event["berakhir"] ?>">
-                    </div>
-                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="berakhir">Jam Berakhir</label>
+                                    <input type="time" id="berakhir" class="form-control" name="berakhir" value="<?= $event["berakhir"] ?>">
+                                </div>
+                                </div>
                 
-                <div class="form-group">
-                    <label for="tempat">Tempat</label>
-                    <input type="text" class="form-control" id="tempat" name="tempat" value="<?= $event["tempat"] ?>">
-                </div>
-                <div class="form-group">
-                    <label for="harga">Harga</label>
-                    <input type="text" class="form-control" id="harga" name="harga" value="<?= $event["harga"] ?>">
-                </div>
-                <legend class="col-form-label ">Benefit</legend>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="benefit" name="benefit[]" value="snacks">
-                    <label class="form-check-label" for="benefit">Snacks</label>
-                </div>
+                                <div class="form-group">
+                                    <label for="tempat">Tempat</label>
+                                    <input type="text" class="form-control" id="tempat" name="tempat" value="<?= $event["tempat"] ?>">
+                                </div>
 
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="benefit" name="benefit[]" value="sertifikat">
-                    <label class="form-check-label" for="benefit">Sertifikat</label>
-                </div>
+                                <div class="form-group">
+                                    <label for="harga">Harga</label>
+                                    <input type="text" class="form-control" id="harga" name="harga" value="<?= $event["harga"] ?>">
+                                </div>
 
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="benefit" name="benefit[]" value="souvenir">
-                    <label class="form-check-label" for="benefit">Souvenir</label>
-                </div>
+                                <legend class="col-form-label ">Benefit</legend>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="benefit" name="benefit[]" value="snacks">
+                                    <label class="form-check-label" for="benefit">Snacks</label>
+                                </div>
 
-                <div class="input-left" >
-                    <button class="btn btn-danger " data-dismiss="modal" style="float: right" name="cancel">cancel</button>
-                    <button type="submit" class="btn btn-primary " style="float: right" name="submit">save changes</button>
-                   
-                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="benefit" name="benefit[]" value="sertifikat">
+                                    <label class="form-check-label" for="benefit">Sertifikat</label>
+                                </div>
 
-            </div>
-            </div>
-            </div>
-            </div>
-        </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="benefit" name="benefit[]" value="souvenir">
+                                    <label class="form-check-label" for="benefit">Souvenir</label>
+                                </div>
+
+                                <div class="input-left" >
+                                    <button class="btn btn-danger " data-dismiss="modal" style="float: right" name="cancel">cancel</button>
+                                    <button type="submit" class="btn btn-primary " style="float: right" name="submit">save changes</button>
+                                </div>
+
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                         </form>
-                    </div>
-      
-                   
+                    </div> 
                 </div>
                
             </div>
